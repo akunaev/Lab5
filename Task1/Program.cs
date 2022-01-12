16656b858955cd84027da8bceb5270fa02fe297b
Аркадий Кунаев <kunayev@mail.ru>
2022-01-12 23:32:13 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Сформировать одномерный массив из 7 элементов. Заполнить массив числами, вводимыми с клавиатуры, определить среднее арифметическое элементов.
+
+double[] array = new double[7];
+double Sum = 0;
+for (int i = 0; i < 7; i++)
+{
+    array[i] = Convert.ToDouble(Console.ReadLine());
+    Sum += array[i];
+}
+Console.WriteLine("Среднее арифметическое равно {0,5:f2}", Sum / 7);
+Console.ReadKey();
